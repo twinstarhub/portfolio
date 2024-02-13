61b693278bcd11c5cb049edbc717290bd25adc49
twinstarhub <akirataro.talent0609@gmail.com>
2024-02-13 23:18:29 +0900

    update

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -10,6 +10,10 @@ import berlitz from "../images/berlitz.png";
 import zetaShop from "../images/zetaShop.png";
 import bayzat from "../images/bayzat.png";
 import Troyz from "../images/Troyz.png";
+import AI_Social_Good from "../images/AI_Social_Good.gif";
+import Plant_AI from "../images/Plant_AI.gif";
+import Face_Recognition from "../images/Face_Recognition.jpeg";
+import Pytorch_CNN_Visualizations from "../images/Pytorch_CNN_Visualizations.png";
 
 const Portfolio = memo(({ refs }) => {
   return (
@@ -95,6 +99,90 @@ const Portfolio = memo(({ refs }) => {
             <a
               className="button"
               href="https://www.berlitz.com/"
+            >
+              publish link
+            </a>
+          </div>
+        </div>
+        <div className="portfolio-info-area">
+          <div className="portfolio-info">
+            <div className="picture-area">
+              <img src={AI_Social_Good} alt="Sorry, failed to load an image." />
+            </div>
+            <div className="title">AI Social Good<br/>Suicidal Ideation Detection In Online Social Content</div>
+            <div className="contents">
+              <br />
+              <b>Descriptoin:</b> The rise of social media and online communities creates safe and anonymous spaces for individuals to share their thoughts about their mental health and express their feelings and sufferings in online communities. To prevent suicide, it is necessary to detect suicide-related posts and user's suicide ideation in cyberspace by natural language processing methods.
+              <br />
+              <br />
+              <b>Tech stacks :</b> GPT4, Tensorflow, Flask, Python, Numpy <br />
+            </div>
+            <a
+              className="button"
+              href="https://www.berlitz.com/"
+            >
+              publish link
+            </a>
+          </div>
+        </div>
+        <div className="portfolio-info-area">
+          <div className="portfolio-info">
+            <div className="picture-area">
+              <img src={Plant_AI} alt="Sorry, failed to load an image." />
+            </div>
+            <div className="title">Plant AI<br/>[Recognition of Plant Diseases by Leaf Image Classification]</div>
+            <div className="contents">
+              <br />
+              <b>Descriptoin:</b> This project is an approach to the development of plant disease recognition model, based on leaf image classification, by the use of deep convolutional networks. The developed model is able to recognize 38 different types of plant diseases out of of 14 different plants with the ability to distinguish plant leaves from their surroundings.
+              <br />
+              <br />
+              <b>Tech stacks :</b> CNN, Tensorflow, Flask, Python <br />
+            </div>
+            <a
+              className="button"
+              href="https://github.com/twinstarhub/Plant_AI"
+            >
+              publish link
+            </a>
+          </div>
+        </div>
+        <div className="portfolio-info-area">
+          <div className="portfolio-info">
+            <div className="picture-area">
+              <img src={Face_Recognition} alt="Sorry, failed to load an image." />
+            </div>
+            <div className="title">Facial Recognition and Emotion Detection</div>
+            <div className="contents">
+              <br />
+              <b>Descriptoin:</b> Humans are used to non verbal communication. The emotions expressed increases the clarity of any thoughts and ideas. It becoms quite interesting when a computer can capture this complex feature of humans, ie emotions. This topic talks about building a model which can detect an emotion from an image. 
+              <br />
+              <br />
+              <b>Tech stacks :</b> Data gathering and augmentation, Model building, Training, Testing <br />
+            </div>
+            <a
+              className="button"
+              href="https://github.com/twinstarhub/Face_Emotion_Detection"
+            >
+              publish link
+            </a>
+          </div>
+        </div>
+        <div className="portfolio-info-area">
+          <div className="portfolio-info">
+            <div className="picture-area">
+              <img src={Pytorch_CNN_Visualizations} alt="Sorry, failed to load an image." />
+            </div>
+            <div className="title">Pytorch CNN Visualizations</div>
+            <div className="contents">
+              <br />
+              <b>Descriptoin:</b> Depending on the technique, the code uses pretrained AlexNet or VGG from the model zoo. Some of the code also assumes that the layers in the model are separated into two sections; features, which contains the convolutional layers and classifier, that contains the fully connected layer (after flatting out convolutions). If you want to port this code to use it on your model that does not have such separation, you just need to do some editing on parts where it calls model.features and model.classifier.
+              <br />
+              <br />
+              <b>Tech stacks :</b> Gradient Visualization, Hierarchical Gradient Visualization, Grad Times Image, Smooth Grad, CNN Filter Visualization, Inverted Image Representations, Deep Dream, Class Specific Image Generation<br />
+            </div>
+            <a
+              className="button"
+              href="https://github.com/twinstarhub/Pytorch_CNN_Visualizations"
             >
               publish link
             </a>
